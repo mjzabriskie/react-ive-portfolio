0cf00b9f6dfc673859e121a46212114e2381bf18
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-07-31 00:34:19 -0600

    created about component

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section>
+            <h1 className='text-center'>Hi, I'm Mark</h1>
+            <article>
+                <p>
+                For the last five years, I've been helping the State of Utah come up with innovative technological solutions to move their workflow from a local mainframe environment to the cloud. My primary task was helping to recreate a new correspondence system through which agents could send correspondence to our customers.
+                </p>
+                <p>
+                    I have recently graduated from the University of Utah Full Stack Coding Bootcamp. This bootcamp reinforced my javascript, HTML5, and CSS skills I cultivated as a state employee, but I also learned the technologies associated with the MERN stack.
+                </p>
+                <p>
+                    Thank you for visiting my portfolio! If you feel like I could help you with a project, please reach out to me via the Contact section.
+                </p>
+            </article>
+        </section>
+    );
+}
+
+export default About;
