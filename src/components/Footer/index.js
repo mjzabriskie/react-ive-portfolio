4a541f293d49053212e2f8fc743cc0c4364a1f2d
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-08-02 01:52:22 -0600

    Broke the footer css while trying to create a glass effect for main, fixed that, and created glass panel for main

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+
 
 function Footer() {
   return (
-    <footer/>
+    <footer className="d-flex flex-wrap justify-content-center align-items-center">
+      <a href="https://github.com/mjzabriskie" target="_blank"></a>
+    <FontAwesomeIcon icon={solid('code-branch')} />
+    </footer>
   );
 }
 
