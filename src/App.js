bf103db8a370c74c2ef883dfeb5a0c494228c2e1
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-08-01 21:23:02 -0600

    Refactored function to reveal different components on App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
 function App() {
-  const [sections] = useState(['About', 'Portfolio', 'Contact', 'Resume']);
+  const [sections] = useState(["About", "Portfolio", "Contact", "Resume"]);
   const [currentSection, setCurrentSection] = useState(sections[0]);
 
-  const showSection = currentSection => {
-    switch (currentSection) {
-      case 'Portfolio':
-        return <Project />;
-      case 'Contact':
-        return <Contact />;
-      case 'Resume':
-        return <Resume />;
-      default:
-        return <About />;
+  const showSection = (currentSection) => {
+    if (currentSection === "Portfolio") {
+      return <Project />;
+    } else if (currentSection === "Contact") {
+      return <Contact />;
+    } else if (currentSection === "Resume") {
+      return <Resume />;
+    } else {
+      return <About />;
     }
-  }
+    // switch (currentSection) {
+    //   case "Portfolio":
+    //     return <Project />;
+    //   case "Contact":
+    //     return <Contact />;
+    //   case "Resume":
+    //     return <Resume />;
+    //   default:
+    //     return <About />;
+    // }
+  };
   return (
     <div className="position-relative min-vh-100 ">
-      <div className='content-wrap'>
-        <Header currentSection={currentSection} setCurrentSection={setCurrentSection} />
-        <main className='pb-5'>
-          {showSection(currentSection)}
-        </main>
+      <div className="content-wrap">
+        <Header
+          currentSection={currentSection}
+          setCurrentSection={setCurrentSection}
+        />
+        <main className="pb-5">{showSection(currentSection)}</main>
       </div>
       <Footer />
     </div>
