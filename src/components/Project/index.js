5f7ea55405b9d8aa7bb619e51f34cfb8f647f45c
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-08-17 18:48:09 -0600

    Added newest project to portfolio, world wide jerky

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,3 +1,4 @@
+import worldWideJerky from "../../assets/images/worldwidejerky.png";
 import marvelousGifs from "../../assets/images/marvel-ous-gifs.png";
 import heroOutline from "../../assets/images/hero-outline.png";
 import runBuddy from "../../assets/images/run-buddy.png";
@@ -10,41 +11,48 @@ const Project = () => {
   const projects = [
     {
       id: 0,
+      title: "World Wide Jerky",
+      img: worldWideJerky,
+      deployed: "https://worldwidejerky.herokuapp.com/",
+      git: "https://github.com/mjzabriskie/world-wide-jerky",
+    },
+    {
+      id: 1,
       title: "Marvel-ous GIFS",
       img: marvelousGifs,
       deployed: "https://mjzabriskie.github.io/marvel-ous-gifs/",
       git: "https://github.com/mjzabriskie/marvel-ous-gifs",
     },
     {
-      id: 1,
+      id: 2,
       title: "A Hero's Outline",
       img: heroOutline,
       deployed: "https://herooutline.herokuapp.com/",
       git: "https://github.com/mjzabriskie/a-heros-outline",
     },
     {
-      id: 2,
+      id: 3,
       title: "Run Buddy",
       img: runBuddy,
       deployed: "https://mjzabriskie.github.io/run-buddy/",
       git: "https://github.com/mjzabriskie/run-buddy",
     },
     {
-      id: 3,
+      id: 4,
       title: "Weather or Not! Weather Dashboard",
       img: weatherDashboard,
       deployed: "https://mjzabriskie.github.io/weather-or-not/",
       git: "https://github.com/mjzabriskie/weather-or-not",
     },
     {
-      id: 4,
+      id: 5,
       title: "Note Taker",
       img: notetaker,
       deployed: "https://application-of-note.herokuapp.com/",
       git: "https://github.com/mjzabriskie/application-of-note",
     },
     {
-      id: 5,
+      id: 6,
       title: "Password Generator",
       img: passwordGenerator,
       deployed: "https://mjzabriskie.github.io/password-generator/",
